45f199eee7885c5e3e225d0dc674b2ec9b55f134
Danilo Andrade <danilotandrade1518@gmail.com>
2024-03-22 16:44:52 -0300

    teste

--- src/domain/usecases/training/create-training-usecase/default-create-training.usecase.spec.ts
+++ src/domain/usecases/training/create-training-usecase/default-create-training.usecase.spec.ts
@@ -1,4 +1,7 @@
-import { CreateTrainingDto } from '@/entities/training/training.entity';
+import {
+  CreateTrainingDto,
+  TrainingEntity,
+} from '@/entities/training/training.entity';
 import { UsecaseError } from '@/usecases/shared/errors/usecase-error';
 import { Either } from '@/utils/either';
 
@@ -10,6 +13,8 @@ import {
 class DefaultCreateTrainingUsecase implements ICreateTrainingUsecase {
   async execute(params: CreateTrainingDto) {
     const either = new Either<UsecaseError, ICreateTrainingUsecaseResult>();
+
+    TrainingEntity.create(params);
 
     either.setData({
       id: null,
@@ -31,5 +36,21 @@ describe('DefaultCreateTrainingUsecase', () => {
     const result = (await createTrainingUsecase.execute(data)).data;
 
     expect(result.id).toBeDefined();
+  });
+
+  it('should call TrainingEntity.create with correct values', async () => {
+    const createTrainingUsecase = new DefaultCreateTrainingUsecase();
+
+    const data = {
+      name: 'Training 1',
+      description: 'Description of training',
+      user: 'user1',
+    };
+
+    const trainingEntitySpy = jest.spyOn(TrainingEntity, 'create');
+
+    (await createTrainingUsecase.execute(data)).data;
+
+    expect(trainingEntitySpy).toHaveBeenCalledWith(data);
   });
 });
