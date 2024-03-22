0af10890bc8c8cff294e4b91cb7f75f69d96adb7
Danilo Andrade <danilotandrade1518@gmail.com>
2024-03-22 16:12:02 -0300

    should be created

--- src/domain/entities/training/training.entity.spec.ts
+++ src/domain/entities/training/training.entity.spec.ts
@@ -1,26 +1,61 @@
 import { Either } from '@/utils/either';
 import { IEntity } from '../entity';
 import { EntityError } from '../shared/errors/entity-error';
+import { randomUUID } from 'crypto';
+
+type CreateTrainingDto = {
+  name: string;
+  description: string;
+  user: string;
+};
 
 class TrainingEntity implements IEntity {
   private _id: string;
+  private _name: string;
+  private _description: string;
+  private _user: string;
+
+  constructor(name: string, description: string, user: string) {
+    this._id = randomUUID();
+    this._name = name;
+    this._description = description;
+    this._user = user;
+  }
 
   get id(): string {
     return this._id;
   }
+  get name(): string {
+    return this._name;
+  }
+  get description(): string {
+    return this._description;
+  }
+  get user(): string {
+    return this._user;
+  }
 
-  static create(data: any): Either<EntityError, TrainingEntity> {
+  static create(data: CreateTrainingDto): Either<EntityError, TrainingEntity> {
     const either = new Either<EntityError, TrainingEntity>();
 
-    either.setData(new TrainingEntity());
+    either.setData(new TrainingEntity(data.name, data.description, data.user));
     return either;
   }
 }
 
 describe('TrainingEntity', () => {
   it('should be created', () => {
-    const trainingEntity = TrainingEntity.create({}).data;
+    const data = {
+      name: 'Training 1',
+      description: 'Description of training',
+      user: 'user1',
+    };
 
-    expect(trainingEntity).toBeDefined();
+    const trainingEntity = TrainingEntity.create(data).data;
+
+    expect(trainingEntity.id).toBeDefined();
+    expect(trainingEntity.name).toEqual(data.name);
+    expect(trainingEntity.description).toEqual(data.description);
+    expect(trainingEntity.user).toEqual(data.user);
   });
 });
